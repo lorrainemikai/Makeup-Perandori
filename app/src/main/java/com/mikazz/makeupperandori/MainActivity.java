dd2611f29f7f039435a2a521e67af2ad6c4482e0
lorraine rono <lorrainerono2017@gmail.com>
2018-07-03 16:50:33 +0300

    added fonts to my activities

--- app/src/main/java/com/mikazz/makeupperandori/MainActivity.java
+++ app/src/main/java/com/mikazz/makeupperandori/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mikazz.makeupperandori;
+    package com.mikazz.makeupperandori;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
--- app/src/main/java/com/mikazz/makeupperandori/SignUpActivity.java
+++ app/src/main/java/com/mikazz/makeupperandori/SignUpActivity.java
@@ -1,13 +1,45 @@
 package com.mikazz.makeupperandori;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
-
+@BindView(R.id.userNameEditTextView)EditText mUserNameEditTextView;
+@BindView(R.id.emailEditText)EditText mEmailEditText;
+@BindView(R.id.firstPasswordEditText)EditText mFirstPasswordEditText;
+@BindView(R.id.secondPasswordEditText)EditText mSecondPasswordEditText;
+@BindView(R.id.loginTextView)TextView mLoginTextView;
+@BindView(R.id.signupButton)Button mSignupButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        mLoginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mUserNameEditTextView.getText().toString();
+                String email = mEmailEditText.getText().toString();
+                String password = mFirstPasswordEditText.getText().toString();
+                String passwordSecond =mSecondPasswordEditText.getText().toString();
+                Intent intent = new Intent(SignUpActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
